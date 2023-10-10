d16b94d1f7cbfc224fb038effd1e9d81f5e2ad8a
Nebojša Koturović <contact@kotur.me>
2023-10-10 22:39:42 +0200

    Simplify macro implementation by using Darling

--- resin-macros/src/lib.rs
+++ resin-macros/src/lib.rs
@@ -1,55 +1,33 @@
 extern crate proc_macro;
 
+use darling::ast::NestedMeta;
+use darling::{Error, FromMeta};
 use proc_macro::TokenStream;
-use quote::{format_ident, quote};
-use syn::{
-    parse::Parser, parse_macro_input, punctuated::Punctuated, Expr, ExprLit, ItemStruct, Lit,
-    LitStr,
-};
+use quote::{quote, format_ident};
+use syn::{parse_macro_input, ItemStruct};
+
+#[derive(Default, Debug, FromMeta)]
+#[darling(default)]
+struct Resin {
+    ops: String,
+}
 
 // using proc_macro_attribute to declare an attribute like procedural macro
 #[proc_macro_attribute]
 pub fn resin_model(args: TokenStream, input: TokenStream) -> TokenStream {
-    let args_parsed =
-        syn::punctuated::Punctuated::<syn::ExprAssign, syn::Token![,]>::parse_terminated
-            .parse(args)
-            .unwrap(); // Better to turn it into a `compile_error!()`
+    let attr_args = match NestedMeta::parse_meta_list(args.into()) {
+        Ok(v) => v,
+        Err(e) => {
+            return TokenStream::from(Error::from(e).write_errors());
+        }
+    };
 
-    let xs: Vec<(String, String)> = args_parsed
-        .iter()
-        .map(|arg| {
-            if let Expr::Path(lhs) = arg.left.as_ref() {
-                let key = lhs.path.segments.first().unwrap().ident.to_string();
-                let value = match key.as_str() {
-                    "ops" => {
-                        if let Expr::Lit(rhs) = arg.right.as_ref() {
-                            if let Lit::Str(rhs_str_lit) = rhs.lit.clone() {
-                                rhs_str_lit
-                                    .token()
-                                    .to_string()
-                                    .strip_prefix("\"")
-                                    .unwrap()
-                                    .strip_suffix("\"")
-                                    .unwrap()
-                                    .to_string()
-                            } else {
-                                panic!("Expected String lit, found {:#?}", rhs);
-                            }
-                        } else {
-                            panic!("Invalid rhs expr {:#?}", arg.right);
-                        }
-                    }
-                    _ => panic!("Invalid key {}", key),
-                };
-                (key, value)
-            } else {
-                panic!("Expected key=val, found {:#?}", arg);
-            }
-        })
-        .collect();
-
-    // println!("{:#?}", xs);
-    let (_, ops_str) = xs.iter().find(|(k, _)| k == "ops").unwrap();
+    let resin = match Resin::from_list(&attr_args) {
+        Ok(v) => v,
+        Err(e) => {
+            return TokenStream::from(e.write_errors());
+        }
+    };
 
     // Parse the input as an item (expecting a struct definition).
     let input = parse_macro_input!(input as ItemStruct);
@@ -57,7 +35,7 @@ pub fn resin_model(args: TokenStream, input: TokenStream) -> TokenStream {
     let input_fields_iter = input.fields.iter();
     let create_struct_name = format_ident!("{}Create", input_struct_name);
 
-    let create_struct = if ops_str.contains('C') {
+    let create_struct = if resin.ops.contains('C') {
         quote! {
             #[derive(Debug, Serialize)]
             pub struct #create_struct_name{
@@ -68,13 +46,10 @@ pub fn resin_model(args: TokenStream, input: TokenStream) -> TokenStream {
         quote!()
     };
 
-    println!("{}", ops_str);
-
     TokenStream::from(quote! {
         #input // don't touch the original struct
 
         use serde::{Serialize,Deserialize};
-        // Create Entity
         #create_struct
     })
 }
