c4ae4f5dcad591e523f92f37692b84de44185e1a
Nebojša Koturović <contact@kotur.me>
2023-10-10 14:35:11 +0200

    Macros: Modify existing macro to generate Create struct

--- resin-macros/src/lib.rs
+++ resin-macros/src/lib.rs
@@ -2,25 +2,79 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
-use syn::{parse_macro_input, ItemStruct};
+use syn::{
+    parse::Parser, parse_macro_input, punctuated::Punctuated, Expr, ExprLit, ItemStruct, Lit,
+    LitStr,
+};
 
 // using proc_macro_attribute to declare an attribute like procedural macro
 #[proc_macro_attribute]
-pub fn resin_entity(_args: TokenStream, input: TokenStream) -> TokenStream {
+pub fn resin_model(args: TokenStream, input: TokenStream) -> TokenStream {
+    let args_parsed =
+        syn::punctuated::Punctuated::<syn::ExprAssign, syn::Token![,]>::parse_terminated
+            .parse(args)
+            .unwrap(); // Better to turn it into a `compile_error!()`
+
+    let xs: Vec<(String, String)> = args_parsed
+        .iter()
+        .map(|arg| {
+            if let Expr::Path(lhs) = arg.left.as_ref() {
+                let key = lhs.path.segments.first().unwrap().ident.to_string();
+                let value = match key.as_str() {
+                    "ops" => {
+                        if let Expr::Lit(rhs) = arg.right.as_ref() {
+                            if let Lit::Str(rhs_str_lit) = rhs.lit.clone() {
+                                rhs_str_lit
+                                    .token()
+                                    .to_string()
+                                    .strip_prefix("\"")
+                                    .unwrap()
+                                    .strip_suffix("\"")
+                                    .unwrap()
+                                    .to_string()
+                            } else {
+                                panic!("Expected String lit, found {:#?}", rhs);
+                            }
+                        } else {
+                            panic!("Invalid rhs expr {:#?}", arg.right);
+                        }
+                    }
+                    _ => panic!("Invalid key {}", key),
+                };
+                (key, value)
+            } else {
+                panic!("Expected key=val, found {:#?}", arg);
+            }
+        })
+        .collect();
+
+    // println!("{:#?}", xs);
+    let (_, ops_str) = xs.iter().find(|(k, _)| k == "ops").unwrap();
+
     // Parse the input as an item (expecting a struct definition).
     let input = parse_macro_input!(input as ItemStruct);
     let input_struct_name = &input.ident;
     let input_fields_iter = input.fields.iter();
-    let model_struct_name = format_ident!("{}Entity", input_struct_name);
+    let create_struct_name = format_ident!("{}Create", input_struct_name);
+
+    let create_struct = if ops_str.contains('C') {
+        quote! {
+            #[derive(Debug, Serialize)]
+            pub struct #create_struct_name{
+                #( pub #input_fields_iter, )*
+            }
+        }
+    } else {
+        quote!()
+    };
+
+    println!("{}", ops_str);
 
     TokenStream::from(quote! {
         #input // don't touch the original struct
 
         use serde::{Serialize,Deserialize};
         // Create Entity
-        #[derive(Debug, Serialize, Deserialize)]
-        pub struct #model_struct_name{
-            #( pub #input_fields_iter, )*
-        }
+        #create_struct
     })
 }
--- resin-macros/tests/macros_test.rs
+++ resin-macros/tests/macros_test.rs
@@ -1,9 +1,9 @@
 // use resin_macros::*;
-use resin_macros::resin_entity;
+use resin_macros::resin_model;
 use serde_json;
 
 // macro resin_model crates a model from this struct
-#[resin_entity]
+#[resin_model(ops = "CRUD")]
 struct User {
     first_name: String,
     last_name: String,
@@ -12,22 +12,22 @@ struct User {
 
 #[test]
 fn test_entity_print_debug() {
-    let user_entity = UserEntity {
+    let user_create = UserCreate {
         first_name: String::from("Jane"),
         last_name: String::from("Doe"),
         age: 31,
     };
-    println!("{:?}", user_entity);
+    println!("{:?}", user_create);
 }
 
 #[test]
 fn test_entity_to_json() -> Result<(), serde_json::Error> {
-    let user_entity = UserEntity {
+    let user_create = UserCreate {
         first_name: String::from("Jane"),
         last_name: String::from("Doe"),
         age: 31,
     };
-    let str = serde_json::to_string(&user_entity)?;
+    let str = serde_json::to_string(&user_create)?;
     assert_eq!(str, r#"{"first_name":"Jane","last_name":"Doe","age":31}"#);
     Ok(())
 }
