7a3f148d19edf9075c6cd8ff9a1f2bf2909445e0
Nebojsa Koturovic <contact@kotur.me>
2024-07-08 13:27:05 +0200

    WIP: Updates to permissions example

--- example/permissions/src/main.rs
+++ example/permissions/src/main.rs
@@ -1,8 +1,10 @@
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 use bitflags::bitflags;
+use std::ops::BitOr;
+use std::ops::BitXor;
 
-#[derive( Debug, Clone, Serialize, Deserialize)]
+#[derive( Default, Debug, Clone, Serialize, Deserialize)]
 pub struct User {
     pub id: Option<Uuid>,
     pub username: Option<String>,
@@ -26,7 +28,7 @@ bitflags! {
     }
 }
 
-#[derive(Default, Clone)]
+#[derive(Default, Debug, Clone)]
 struct UserPermissions {
     pub model: CrudPermissions,
     pub id: CrudPermissions,
@@ -40,18 +42,54 @@ struct UserPermissions {
     pub language: CrudPermissions,
 }
 
+impl BitOr for UserPermissions {
+    type Output = UserPermissions;
+    fn bitor(self : Self, rhs : Self) -> Self {
+        UserPermissions {
+            model: self.model | rhs.model,
+            id: self.id | rhs.id,
+            username: self.username | rhs.username,
+            email: self.email | rhs.email,
+            password: self.password | rhs.password,
+            first_name: self.first_name | rhs.first_name,
+            last_name: self.last_name | rhs.last_name,
+            date_of_birth: self.date_of_birth | rhs.date_of_birth,
+            country: self.country | rhs.country,
+            language: self.language | rhs.language
+        }
+    }
+}
 
-enum Group {
-    Guest,
-    User,
-    Moderator,
-    Admin,
-    Owner,
+impl BitXor for UserPermissions {
+    type Output = UserPermissions;
+    fn bitxor(self : Self, rhs : Self) -> Self {
+        UserPermissions {
+            model: self.model ^ rhs.model,
+            id: self.id ^ rhs.id,
+            username: self.username ^ rhs.username,
+            email: self.email ^ rhs.email,
+            password: self.password ^ rhs.password,
+            first_name: self.first_name ^ rhs.first_name,
+            last_name: self.last_name ^ rhs.last_name,
+            date_of_birth: self.date_of_birth ^ rhs.date_of_birth,
+            country: self.country ^ rhs.country,
+            language: self.language ^ rhs.language
+        }
+    }
 }
 
 
+// enum Group {
+//     Guest,
+//     User,
+//     Moderator,
+//     Admin,
+//     Owner,
+// }
+
+
 #[derive(Default)]
-struct GroupPermissions<P: Default> {
+struct GroupPermissions<P: Default + BitOr + BitXor> {
     guest : P,
     user : P,
     moderator : P,
@@ -59,8 +97,21 @@ struct GroupPermissions<P: Default> {
     owner : P,
 }
 
+impl <P> BitOr for GroupPermissions<P> where P: Default + BitOr<Output = P> + BitXor<Output = P> {
+    type Output = GroupPermissions<P>;
+    fn bitor(self : Self, rhs : GroupPermissions<P>) -> GroupPermissions<P> {
+        GroupPermissions::<P> {
+            guest: self.guest | rhs.guest,
+            user: self.user | rhs.user,
+            admin: self.admin | rhs.admin,
+            moderator: self.moderator | rhs.moderator,
+            owner: self.owner | rhs.owner,
+        }
+    }
+}
+
 trait ModelPermissions {
-    type Permissions : Default;
+    type Permissions : Default + BitOr + BitXor;
     fn permissions() -> GroupPermissions<Self::Permissions>;
 }
 
@@ -94,21 +145,36 @@ impl ModelPermissions for User {
             moderator: moderator_permissions,
             ..GroupPermissions::default()
         }
-
     }
 }
 
-fn check_permissions<M : ModelPermissions>(requested_permissions : M::Permissions, groups : GroupPermissions<bool>) -> bool {
+fn check_permissions<M : ModelPermissions>(requested_permissions : M::Permissions, groups : GroupPermissions<bool>) -> M::Permissions
+where <M as ModelPermissions>::Permissions: BitOr<Output=M::Permissions> + BitXor<Output=M::Permissions> {
     let model_permissions = M::permissions();
     let mut given_permissions : M::Permissions = M::Permissions::default();
     if groups.user {
-        given_permissions = model_permissions.user
+        given_permissions = given_permissions | model_permissions.user
     }
-
-    false
+    given_permissions ^ requested_permissions
 }
 
 
 
 fn main() {
+
+    let u = User::default();
+    let result = check_permissions::<User>(
+        UserPermissions{ 
+            first_name: CrudPermissions::READ,
+            ..UserPermissions::default()
+        },
+        GroupPermissions {
+            guest: false,
+            user: true,
+            ..GroupPermissions::default()
+        }
+    );
+
+    println!("{:?}", result);
+
 }
